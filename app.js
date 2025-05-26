86d8a02638070fea136796cc14e7af09071e8e40
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-05-26 14:09:29 +0300

    Refactor 2.

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-const logger = require('./utils/logger')
-
+const express = require('express')
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
-const Blog = mongoose.model('Blog', blogSchema)
+mongoose.set('strictQuery', false)
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
