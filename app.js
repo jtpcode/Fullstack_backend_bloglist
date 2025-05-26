6b09e76ede0f85228c42ad1fefa19702b4abff55
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-05-26 14:25:50 +0300

    E4.2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -14,8 +15,17 @@ mongoose.connect(config.MONGODB_URI)
     logger.info('connected to MongoDB')
   })
 
+// Ennen routeja rekisteröidään käyttöön:
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
+// Sitten routet mukaan, kun ylläolevat on käytössä
 app.use('/api/blogs', blogsRouter)
+
+// Viimeiseksi väärä-url ja virheiden käsittely, jos
+// oikeaa routea ei löytynyt tai heitettiin virhe
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
