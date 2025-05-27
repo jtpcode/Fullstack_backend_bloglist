951f544142170a757f5768f3f894093dde02826f
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-05-27 13:16:01 +0300

    E4.4

---
+++ tests/favoriteBlog.test.js
@@ -0,0 +1,20 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+
+const favoriteBlog = require('../utils/list_helper').favoriteBlog
+const { blogs } = require('../utils/bloglist')
+
+describe('favorite blog', () => {
+  const mostLikedBlog = {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  }
+
+  test('with multiple blogs', () => {
+    assert.deepStrictEqual(favoriteBlog(blogs), mostLikedBlog)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,13 +7,26 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
-    return sum + item['likes']
+    return sum + item.likes
   }
 
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (favBlog, blog) => {
+    if (blog.likes > favBlog.likes) {
+      return blog
+    }
+    return favBlog
+  }
+
+  return blogs.reduce(reducer)
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
