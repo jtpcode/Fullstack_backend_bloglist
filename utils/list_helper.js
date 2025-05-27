90e6acb740f34afd18c7bb67f779166709f8f5d9
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-05-27 11:01:12 +0300

    E4.3-4

---
+++ utils/list_helper.js
@@ -0,0 +1,19 @@
+const dummy = (blogs) => {
+  if (blogs) {
+    return 1
+  }
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item['likes']
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+}
